f93aedcfc488f19d18282a366082ef5604bb241f
dasdfadssda <quddkflty@gmail.com>
2024-05-05 15:57:29 +0900

    Initial commit

---
+++ src/Page/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AppAbout from "../Components/App/App_About_Components/AppAbout";
+
+const AboutPage = () => {
+
+  return (
+    <>
+    <div>
+          <AppAbout />
+        </div>
+    </>
+  );
+};
+
+export default AboutPage;
---
+++ src/Page/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AppHome from "../Components/App/App_Home_Components/AppHome";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        <AppHome />
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
