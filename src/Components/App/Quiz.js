97c887cc7adb069c0386a1dcb7cdb9d5531d001f
dasdfadssda <quddkflty@gmail.com>
2024-05-06 23:22:23 +0900

    hosting

--- src/Components/App/Quiz.js
+++ src/Components/App/Quiz.js
@@ -64,7 +64,7 @@ const NextButton = styled.button`
 
 const Modal = styled.div`
   position: fixed;
-  top: 40%;
+  top: 45%;
   left: 50%;
   transform: translate(-50%, -50%);
   display: ${(props) => (props.show ? "block" : "none")};
