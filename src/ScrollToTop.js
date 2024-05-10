bbdef6479bca16a6d16802d909885b96e58ad0d4
dasdfadssda <quddkflty@gmail.com>
2024-05-10 10:16:35 +0900

    update

--- src/ScrollToTop.js
+++ src/ScrollToTop.js
@@ -5,7 +5,7 @@ function ScrollToTop() {
   const { pathname } = useLocation(); 
 
   useEffect(() => {
-    window.scrollTo(0, 0); // 페이지 이동시 페이지 상단으로 이동
+    window.scrollTo(0, document.body.scrollHeight);
   }, [pathname]);
 
   return null;
